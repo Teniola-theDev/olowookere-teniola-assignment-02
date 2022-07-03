28690691c4abad0bf27a0cab4b3bae4b6847b1fa
Teniola Olowookere <>
2022-07-03 14:48:02 +0000

    attempt 1

--- src/assignment.js
+++ src/assignment.js
@@ -13,9 +13,11 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
+let arrayOfNumbers = [1, 2, 3, 4, 5];
+let sumOfNumbers = arrayOfNumbers.reduce(function (a, b) {
+  return a + b;
+}, 0);
+console.log(sumOfNumbers);
 // assignment.sumOfNumbers = sumOfNumbers;
 
 /**
@@ -27,9 +29,9 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
+// function countEvenNumbers(arrayOfNumbers) {
+//     return arrayOfNumbers.length;
+// }
 // assignment.countEvenNumbers = countEvenNumbers;
 
 /**
@@ -46,9 +48,9 @@ function countEvenNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
-function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
-}
+// function celsiusToFahrenheit(arrayOfNumbers) {
+//     return arrayOfNumbers;
+// }
 // assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
